c4dc497deef2401f53d340f0e96066703bc08060
Wendel Fernandes de Lana <48734976+WendelLana@users.noreply.github.com>
2023-01-09 15:45:08 -0300

    Add files via upload

---
+++ scripts/animations.js
@@ -0,0 +1,23 @@
+
+flag = 1
+        function f1() {
+            document.getElementById("imagem").src = "https://media.giphy.com/media/gLugCaElYXH7589sSb/giphy.gif"
+            document.getElementById("texto").textContent = "Tá marcado amanhã às 18h"
+            alert("YEAH, TE AMO! <3")
+            document.getElementById("By").style.visibility = "hidden"
+            document.getElementById("Bn").style.visibility = "hidden"
+            document.getElementById("heart").style.display = "block"
+        }
+        function f() {
+            let By = document.getElementById("By")
+            By.style.margin = 0
+            By.style.padding = 0
+            By.style.position = "absolute"
+            let Bn = document.getElementById("Bn")
+            Bn.style.position = "absolute"
+            document.getElementById("imagem").src = "https://i.gifer.com/HOFK.gif"
+
+            Bn.style.top = 520 + Math.random() * 200
+            Bn.style.left = 220 + Math.random() * 900
+        }
+
